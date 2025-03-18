04610af866b4793b93c66a4f7f0951ceb7f0e67b
hajricod <48984962+hajricod@users.noreply.github.com>
2025-03-18 08:48:14 +0400

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Enums\UserRole;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +16,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@cityeye.com',
+            'password' => Hash::make('password'),
+            'role' => UserRole::Admin,
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10),
         ]);
+
+        User::factory(10)->create();
     }
 }
