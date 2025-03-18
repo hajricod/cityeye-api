922650870cee5d0aabe98e09d68e82d91b3402ff
hajricod <48984962+hajricod@users.noreply.github.com>
2025-03-18 09:25:08 +0400

    updates

--- routes/api.php
+++ routes/api.php
@@ -8,14 +8,13 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
+
 Route::prefix('v1/')->group(function () {
     Route::middleware([BasicAuthMiddleware::class])->prefix('admin')->group(function () {
         Route::resource('users', UsersController::class);
     });
     Route::post('register', [AuthController::class, 'register']);
 });
-
-
 
 Route::middleware(BasicAuthMiddleware::class)->get('/user', function () {
     return response()->json(['message' => 'Authenticated successfully']);
